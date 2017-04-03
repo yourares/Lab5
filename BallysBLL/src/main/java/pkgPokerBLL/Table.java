00245bafffb36709177ae6e9c7e323256cc69d35
tstbag <tstbag@verizon.net>
2017-04-03 11:15:42 -0400

    Initial commit

---
+++ BallysBLL/src/main/java/pkgPokerBLL/Table.java
@@ -0,0 +1,30 @@
+package pkgPokerBLL;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Table implements Serializable {
+
+	private UUID TableID;
+	
+	//	Change this from ArrayList to HashMap.
+	private ArrayList<Player> TablePlayers = new ArrayList<Player>();
+	
+	public Table() {
+		super();
+		TableID = UUID.randomUUID();
+	}
+	
+	public Table AddPlayerToTable(Player p)
+	{
+		//TODO: Add a player to the table (should be pretty easy)		
+		return this;
+	}
+	
+	public Table RemovePlayerFromTable(Player p)
+	{
+		//TODO: Remove a player from the table
+		return this;
+	}
+}
